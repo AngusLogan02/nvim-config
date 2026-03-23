5d0840f1c72ef640c77f14c1f7feb05770dc5446
Angus Logan <angus.logan@baronsview.com>
2026-03-23 22:40:49 +0000

    initial commit

---
+++ lua/nconf/lazy_plugins/treesitter.lua
@@ -0,0 +1,22 @@
+return {
+  'nvim-treesitter/nvim-treesitter',
+  lazy = false,
+  build = ':TSUpdate',
+
+  config = function()
+    require('nvim-treesitter').install({
+      'lua',
+      'python',
+    })
+
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = {
+        'lua',
+        'python',
+      },
+      callback = function()
+        vim.treesitter.start()
+      end,
+    })
+  end,
+}
---
+++ lua/nconf/remap.lua
@@ -0,0 +1,7 @@
+vim.keymap.set("i", "jk", "<Esc>")
+vim.keymap.set("n", "-", "<CMD>Oil<CR>")
+
+vim.keymap.set("n", "<Leader>h", "<C-w>h")
+vim.keymap.set("n", "<Leader>j", "<C-w>j")
+vim.keymap.set("n", "<Leader>k", "<C-w>k")
+vim.keymap.set("n", "<Leader>l", "<C-w>l")
