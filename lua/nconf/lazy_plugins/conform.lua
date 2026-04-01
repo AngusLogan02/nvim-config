07b0ad10c45a2310e848b1bc6d79fe58137945ff
Angus Logan <angus.logan@sse.com>
2026-04-01 12:38:20 +0100

    fix: conform gofmt

--- lua/nconf/lazy_plugins/conform.lua
+++ lua/nconf/lazy_plugins/conform.lua
@@ -1,24 +1,25 @@
 return {
-  'stevearc/conform.nvim',
+	"stevearc/conform.nvim",
 
-  config = function()
-    require('conform').setup({
-      formatters_by_ft = {
-        lua = { 'stylua', stop_after_first = true },
-        javascript = { 'prettier', stop_after_first = true },
-        typescript = { 'prettier', stop_after_first = true },
-        javascriptreact = { 'prettier', stop_after_first = true },
-        typescriptreact = { 'prettier', stop_after_first = true },
-        css = { 'prettier', stop_after_first = true },
-        nix = { 'nixfmt', stop_after_first = true },
-      },
-    })
+	config = function()
+		require("conform").setup({
+			formatters_by_ft = {
+				lua = { "stylua", stop_after_first = true },
+				javascript = { "prettier", stop_after_first = true },
+				typescript = { "prettier", stop_after_first = true },
+				javascriptreact = { "prettier", stop_after_first = true },
+				typescriptreact = { "prettier", stop_after_first = true },
+				css = { "prettier", stop_after_first = true },
+				nix = { "nixfmt", stop_after_first = true },
+				go = { "gofmt", stop_after_first = true },
+			},
+		})
 
-    vim.api.nvim_create_autocmd('BufWritePre', {
-      pattern = '*',
-      callback = function(args)
-        require('conform').format({ bufnr = args.buf })
-      end,
-    })
-  end,
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			pattern = "*",
+			callback = function(args)
+				require("conform").format({ bufnr = args.buf })
+			end,
+		})
+	end,
 }
