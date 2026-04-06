2dabf6d98ca1519c35b1e802076689feebc23c82
Angus Logan <angus.logan@sse.com>
2026-04-06 15:41:09 +0100

    word wrap, list

--- lua/nconf/set.lua
+++ lua/nconf/set.lua
@@ -5,8 +5,9 @@ vim.opt.expandtab = true
 vim.opt.autoindent = true
 vim.opt.smartindent = true
 vim.opt.smarttab = true
-vim.opt.list = true
+-- vim.opt.list = true
 -- vim.opt.listchars = "eol:.,tab:>-,trail:~,extends:>,precedes:<"
+vim.opt.wrap = false
 
 vim.opt.number = true
 vim.opt.relativenumber = true
